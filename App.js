46ca51b71b9993e0674537aa0e142f71c4a8e4b1
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-04-27 11:46:58 +0530

    added some components

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    {id:"heading"},
-    "Hello World "
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img
+          src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-express-by-gigih-rudya-designcrowd.png"
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Cart</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// const data = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },[
-//     React.createElement("h1", {}, "i am h1 tag"),
-//     React.createElement("h1", {}, "i am h1 tag")]
-//   )
-// );
+const RestorentCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"
+        alt="res-logo"
+      />
+      <h3>Mekhana Foods</h3>
+      <h4>Biriyani,North Indian, Asian</h4>
+      <h4>4.4 star</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+      </div>
+    </div>
+  );
+};
+const Applayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Applayout />);
