1cb4838dc8ad250dc464620dabc1dbcce90867a8
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-05-06 09:10:54 +0530

    removed the comment

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,7 +15,7 @@ const Body = () => {
 
   console.log("body rendered "); 
 
-  //fetching data using live api
+
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
